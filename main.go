44f4d3949f433719ee2c22153f008c7f4ae7855b
VladDrag <vdragomir@protonmail.com>
2023-03-01 11:53:16 +0100

    clean up redundant code

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-    "fmt"
     "net/http"
 )
 
 func main() {
 
-    http.Handle("/s/", http.FileServer(http.Dir("./")))
+    http.Handle("/", http.FileServer(http.Dir("")))
     http.ListenAndServe(":8080", nil)
-}
-
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }

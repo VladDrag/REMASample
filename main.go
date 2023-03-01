e12ef46d05e9f8b6a640e37585dbbb1a1ef58f37
Vlad Dragomir <vladdragomir@Vlads-MacBook-Pro.local>
2023-03-01 09:27:39 +0100

    modified main

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
+// package main
+
+// import (
+// 	"net/http"
+// )
+
+
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		http.ServeFile(w, r, "./index.html")
+// 	})
+// 	http.ListenAndServe(":8080", nil)
+// }
+
 package main
 
 import (
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./index.html")
-	})
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":8080", nil)
 }
 
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}

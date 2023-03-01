f848d7335f2f4f64005f86f81ff314b2a768ed0d
VladDrag <vdragomir@protonmail.com>
2023-03-01 11:30:18 +0100

    Update Handle func path

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-    http.Handle("/", http.FileServer(http.Dir("./s/")))
+    http.Handle("/s/", http.FileServer(http.Dir("./")))
     http.ListenAndServe(":8080", nil)
 }
 

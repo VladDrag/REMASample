5a6d380b0a2a298b4a0dc2472f6b3c87fd74ca1d
Vlad Dragomir <vladdragomir@Vlads-MacBook-Pro.local>
2023-03-01 09:16:37 +0100

    modified path to index

--- main.go
+++ main.go
@@ -7,11 +7,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+		http.ServeFile(w, r, "./index.html")
 	})
 	http.ListenAndServe(":8080", nil)
 }
 
-// func HelloServer(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Hello , %s!", r.URL.Path[1:])
-// }

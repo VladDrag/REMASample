68d9d7320bf9e5bfcba7c5ad6918a64eb700624d
VladDrag <vdragomir@protonmail.com>
2023-03-02 08:03:25 +0100

    Revert "clean up redundant code"
    This reverts commit 44f4d3949f433719ee2c22153f008c7f4ae7855b.

--- main.go
+++ main.go
@@ -52,3 +52,7 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("")))
 	http.ListenAndServe(":8080", nil)
 }
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
